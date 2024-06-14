ac92e4e1232d3cbb3aad141193502af86ce25171
Jan Eris <janerissaludo@gmail.com>
2024-06-14 23:20:08 +0800

    resolving deployment issue

--- src/app/components/shop/CardList.tsx
+++ src/app/components/shop/CardList.tsx
@@ -8,34 +8,13 @@ function CardList() {
   <div className=' p-2 m-4 w-[280px]  rounded-md hover:cursor-pointer'>
         <div ><Image   src={"/shop/Af1-Black.jpg"} width={500} height={500} alt=''/></div>        
         <div className='mt-2 p-2 '>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
             <h1 className='font-semibold' >Nike Air Force 1 &apos;07 EasyOn</h1>
             <p>₱ 6,999</p>
         </div>       
     </div>
 
-    <div className=' p-2 m-4 w-[280px]  rounded-md hover:cursor-pointer'>
-        <div ><Image   src={"/shop/Af1-Black.jpg"} width={500} height={500} alt=''/></div>        
-        <div className='mt-2 p-2 '>
-            <h1 className='font-semibold' >Nike Air Force 1 &apos;07 EasyOn</h1>
-            <p>₱ 6,999</p>
-        </div>       
-    </div>
-
-    <div className=' p-2 m-4 w-[280px]  rounded-md hover:cursor-pointer'>
-        <div ><Image   src={"/shop/Af1-Black.jpg"} width={500} height={500} alt=''/></div>        
-        <div className='mt-2 p-2 '>
-            <h1 className='font-semibold' >Nike Air Force 1 &apos;07 EasyOn</h1>
-            <p>₱ 6,999</p>
-        </div>       
-    </div>
-
-    <div className=' p-2 m-4 w-[280px]  rounded-md hover:cursor-pointer'>
-        <div ><Image   src={"/shop/Af1-Black.jpg"} width={500} height={500} alt=''/></div>        
-        <div className='mt-2 p-2 '>
-            <h1 className='font-semibold' >Nike Air Force 1 &apos;07 EasyOn</h1>
-            <p>₱ 6,999</p>
-        </div>       
-    </div>
+    
   </div>
    
 
